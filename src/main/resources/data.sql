0f03c3065ba711853c85edb3469c2c01962a88c2
Виталий Сазанов <zzzz95515@gmail.com>
2022-07-12 23:21:42 +0300

    postgres appended

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,25 +1,3 @@
-insert into authors (id, first_name, last_name) values (1, 'Aldwin', 'Maddison');
-insert into authors (id, first_name, last_name) values (2, 'Marlene', 'Claydon');
-insert into authors (id, first_name, last_name) values (3, 'Maurits', 'Brende');
-insert into authors (id, first_name, last_name) values (4, 'Constantine', 'Garton');
-insert into authors (id, first_name, last_name) values (5, 'Kali', 'Barends');
-insert into authors (id, first_name, last_name) values (6, 'Courtnay', 'Farleigh');
-insert into authors (id, first_name, last_name) values (7, 'Yorke', 'MacDwyer');
-insert into authors (id, first_name, last_name) values (8, 'Cristiano', 'Peasey');
-insert into authors (id, first_name, last_name) values (9, 'Tod', 'Woolaston');
-insert into authors (id, first_name, last_name) values (10, 'Jen', 'Cunney');
-insert into authors (id, first_name, last_name) values (11, 'Shalom', 'Rodbourne');
-insert into authors (id, first_name, last_name) values (12, 'Collin', 'Spaughton');
-insert into authors (id, first_name, last_name) values (13, 'Honoria', 'Matthesius');
-insert into authors (id, first_name, last_name) values (14, 'Daron', 'Scarce');
-insert into authors (id, first_name, last_name) values (15, 'Khalil', 'Arnull');
-insert into authors (id, first_name, last_name) values (16, 'Tisha', 'Dauber');
-insert into authors (id, first_name, last_name) values (17, 'Tedd', 'Burles');
-insert into authors (id, first_name, last_name) values (18, 'Randie', 'Andreas');
-insert into authors (id, first_name, last_name) values (19, 'Karlene', 'Granger');
-insert into authors (id, first_name, last_name) values (20, 'Major', 'Mattersley');
-insert into authors (id, first_name, last_name) values (21, 'Hatti', 'Skelbeck');
-
 insert into books (id, price, price_old, title, author_id) values (1, 1642, 1843, 'Bitwolf', 37);
 insert into books (id, price, price_old, title, author_id) values (2, 707, 1451, 'Tres-Zap', 66);
 insert into books (id, price, price_old, title, author_id) values (3, 1590, 2445, 'Daltfresh', 80);
